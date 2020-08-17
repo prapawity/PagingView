d66e5aaded2fcd7bc19826849f39a9fe3c3eb9ab
prapawit patthasirivichot <60070045@kmitl.ac.th>
2020-08-17 10:45:56 +0700

    first commit

---
+++ PagingPractice/ViewControllersFactory.swift
@@ -0,0 +1,9 @@
+//
+//  ViewControllersFactory.swift
+//  PagingPractice
+//
+//  Created by Prapawit Patthasirivichot on 17/8/2563 BE.
+//  Copyright © 2563 Prapawit Patthasirivichot. All rights reserved.
+//
+
+import Foundation
